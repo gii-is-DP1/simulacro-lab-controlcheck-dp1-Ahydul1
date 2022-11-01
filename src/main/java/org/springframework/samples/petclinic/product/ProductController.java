3f5ebdf9f20520f80cd4782ad50737ca46284fd9
Ahydul1 <ahydul1@gmail.com>
2022-11-01 17:45:15 +0100

    entrega simulacro

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,58 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
-    
+	
+	private static final String PRODUCT_FORM = "products/createOrUpdateProductForm";
+	private static final String WELCOME = "welcome";
+	private ProductService ps;
+
+	@Autowired
+	public ProductController(ProductService ps) {
+		this.ps = ps;
+	}
+	
+	@GetMapping("/create")
+	public ModelAndView añadeProducto() {
+		ModelAndView res = new ModelAndView(PRODUCT_FORM);
+		Product product = new Product();
+		res.addObject("product", product);
+		res.addObject("productTypes", ps.findAllProductTypes());
+
+		return res;
+	}
+	@PostMapping("/create")
+	public ModelAndView añadeProducto(@Valid Product producto, BindingResult result) {
+		ModelAndView res = new ModelAndView(PRODUCT_FORM);
+		res.addObject("productTypes", ps.findAllProductTypes());
+		res.addObject("product", producto);
+		
+		if (result.hasErrors()) {
+			res.addObject("error", "Error de validacion");
+		}
+		else {
+            try{
+            	ps.save(producto);
+    			return new ModelAndView(WELCOME);
+            }catch(DataAccessException e){
+    			res.addObject("error", "Error guardando producto");
+                return res;
+            }
+		}
+		
+		return res;
+	}
+
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,28 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, Integer>{
     List<Product> findAll();
+    
+	@Query("SELECT pt FROM ProductType pt")
     List<ProductType> findAllProductTypes();
+	
     Optional<Product> findById(int id);
+    
+	@Query("SELECT pt FROM ProductType pt WHERE pt.name = :name")
+    ProductType findProductTypeByName( @Param("name") String name);
+
     Product findByName(String name);
-    Product save(Product p);
+    
+    Product save(Product p) throws DataAccessException;
+    
+    @Query("SELECT p FROM Product p WHERE p.price < :coste")
+    List<Product> findByPriceLessThan(@Param("coste") Double coste);
 }
